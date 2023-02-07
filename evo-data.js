33b6487112c23e8d902326603585dddb55d9a456
anhminh362 <116275383+anhminh362@users.noreply.github.com>
2023-02-07 13:14:11 +0700

    Exercise

---
+++ evo-data.js
@@ -0,0 +1,56 @@
+var Product=[
+    {
+        status:1,
+        name:"Bánh Cracker KFC kinh đô vị Phô mai(8 gói x 25g)",
+        price:"<del>35.000₫</del>"+" "+ " 29.800₫ ",
+        image:"https://bizweb.dktcdn.net/thumb/large/100/366/378/products/9-2ae7523d-fdb0-4caf-bc36-12c51b43f1fc.png?v=1568946112757",
+    },
+    {
+        status:1,
+        name:"Hộp 10 gói bánh King Đô vị Oreo mini chocolate 260g",
+        price:" 54.000₫ ",
+        image:"https://bizweb.dktcdn.net/thumb/large/100/366/378/products/10-2854416b-a234-4c35-9a87-4ae84d475cf4.png?v=1568944833700",
+    },
+    {
+        status:1,
+        name:"Bánh Trứng Tiktok sầu riêng(120g)",
+        price:" 23.000₫ ",
+        image:"https://bizweb.dktcdn.net/thumb/large/100/366/378/products/12-305c5dfc-8a9a-4fe5-ba33-c4b04e154a8c.png?v=1568944752707",        
+    },
+    {
+        status:1,
+        name:"Bánh gấu chùm ngây nhân kem(200g)",
+        price:"<del>32.000₫</del>"+" "+ "28.000₫ ",
+        image:"https://bizweb.dktcdn.net/thumb/large/100/366/378/products/8.png?v=1568944624590",
+    },
+    {
+        status:1,
+        name:"Bánh bơ Ricky trứng(270g)",
+        price:" 29.800₫ ",
+        image:"https://bizweb.dktcdn.net/thumb/large/100/366/378/products/7.png?v=1568944398257",
+    },
+    {
+        status:1,
+        name:"Bánh gạo tokbokki (140g)",
+        price:" 39.000₫ ",
+        image:"https://bizweb.dktcdn.net/thumb/large/100/366/378/products/6.png?v=1568944235997",
+    },
+    
+    {
+        status:2,
+        image:"https://bizweb.dktcdn.net/100/366/378/themes/736342/assets/evo_gift_image_1.jpg?1663905428530",
+    },
+    {
+        status:2,
+        image:"https://bizweb.dktcdn.net/100/366/378/themes/736342/assets/evo_gift_image_2.jpg?1663905428530",
+    },
+    {
+        status:2,
+        image:"https://bizweb.dktcdn.net/100/366/378/themes/736342/assets/evo_gift_image_3.jpg?1663905428530",
+    },
+    {
+        status:2,
+        image:"https://bizweb.dktcdn.net/100/366/378/themes/736342/assets/evo_gift_image_4.jpg?1663905428530",
+    }
+    
+]
